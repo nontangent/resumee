ae31f9bceab73e1edff1444b5e1ee0e5000e2474
Nozomu Miyamoto <nontan@NozomunoMacBook-ea.local>
2020-04-15 06:12:09 +0900

    スペルミス修正

--- app/src/server/settings.ts
+++ app/src/server/settings.ts
@@ -10,12 +10,12 @@ export const PORT = process.env.PORT || 4000;
 export const DIST_DIR = join(process.cwd(), 'dist/resumee/browser');
 export const UPLOAD_DIR = join(DIST_DIR, 'assets/resumes');
 
-export const GOOGLE_CREDIENCIAL = jsonFromBase64(process.env.GOOGLE_CREDIENCIAL_JSON_BASE64);
+export const GOOGLE_CREDENTIAL = jsonFromBase64(process.env.GOOGLE_CREDENTIAL_JSON_BASE64);
 
-// console.log('GOOGLE_CREDIENCIAL:', GOOGLE_CREDIENCIAL);
-console.log('GOOGLE_CREDIENCIAL:', GOOGLE_CREDIENCIAL.client_email);
-console.log('GOOGLE_CREDIENCIAL:', GOOGLE_CREDIENCIAL.private_key);
-export const GOOGLE_CLIENT_EMAIL = GOOGLE_CREDIENCIAL.client_email;
-export const GOOGLE_PRIVATE_KEY = GOOGLE_CREDIENCIAL.private_key;
+// console.log('GOOGLE_CREDENTIAL:', GOOGLE_CREDENTIAL);
+console.log('GOOGLE_CREDENTIAL:', GOOGLE_CREDENTIAL.client_email);
+console.log('GOOGLE_CREDENTIAL:', GOOGLE_CREDENTIAL.private_key);
+export const GOOGLE_CLIENT_EMAIL = GOOGLE_CREDENTIAL.client_email;
+export const GOOGLE_PRIVATE_KEY = GOOGLE_CREDENTIAL.private_key;
 
 export const GOOGLE_ANALYTICS_VIEW_ID = process.env.GOOGLE_ANALYTICS_VIEW_ID;
